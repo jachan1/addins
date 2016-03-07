0696ca94d0f56286c6ecd0c451b0b8e6e666102e
jachan1 <jachan1@gmail.com>
2016-03-07 11:29:22 -0500

    need to load rstudioapi to use getActiveDocumentContext

--- R/fixCommas.R
+++ R/fixCommas.R
@@ -3,7 +3,7 @@
 #' Call this function to format all commas selected with one space before and one after
 #' 
 #' @export
-
+require(rstudioapi)
 fixCommas <- function() {
   seltxt <- getActiveDocumentContext()
   rstudioapi::insertText(gsub(" *, *", ", ", seltxt$selection[[1]]$text))
