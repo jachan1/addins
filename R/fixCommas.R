cf3868c3921d7d23676690bda74eff42100c5b0f
jachan1 <jachan1@gmail.com>
2016-03-08 15:48:26 -0500

    comma fix

--- R/fixCommas.R
+++ R/fixCommas.R
@@ -3,8 +3,9 @@
 #' Call this function to format all commas selected with one space before and one after
 #' 
 #' @export
-require(rstudioapi)
+
 fixCommas <- function() {
+  require(rstudioapi)
   seltxt <- getActiveDocumentContext()
   rstudioapi::insertText(gsub(" *, *", ", ", seltxt$selection[[1]]$text))
 }
