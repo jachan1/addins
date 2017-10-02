93cd824ab77c8a073c9d61d0338a5c449352f594
jachan1 <jachan1@gmail.com>
2017-10-02 11:02:11 -0400

    added a block quote function to use with markdown

---
+++ R/mkdnBlockQuote.R
@@ -0,0 +1,13 @@
+#' add block quotes
+#'
+#' add the prefix "> " to each line to create markdown block quotes.
+#' 
+#' @export
+
+mkdnBlockQuote <- function() {
+  seltxt <- rstudioapi::getActiveDocumentContext()
+  txt <- seltxt$selection[[1]]$text
+  bq_txt <- paste("> ", strsplit(tmp, "\n")[[1]], collapse="\n")
+  rstudioapi::insertText(bq_txt)
+}
+
